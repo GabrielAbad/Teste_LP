d0c4a550d4a1d0a112e1310fe973a45c73c8fc46
GABRIEL RODRIGUES DE DEUS ABAD <pedroabadg961@gmail.com>
2023-09-18 09:11:05 -0300

    Finalizando a terceira função

--- exec.py
+++ exec.py
@@ -1,4 +1,8 @@
-from utils import *
+from utils import parse_date, time_delta, extract_lines
 
-print(parse_date("34 de Agosto de 2023"))
-print(time_delta(data_1,data_2))
+print(parse_date("30 de Agosto de 2023"))
+print(time_delta('18 de Janeiro de 2023','18 de Janeiro de 2024'))
+
+l_file = extract_lines('input_text.txt')[0]
+l_file2 = extract_lines('input_text.txt')[1]
+print(time_delta(l_file,l_file2))
--- utils.py
+++ utils.py
@@ -33,6 +33,7 @@ def parse_date(date:str)-> list:
 
     """
     list_parsed_string = date.split(sep=' ')
+    date = date.strip()
     try:    
         if int(list_parsed_string[0]) > 31:
             raise ValueError
@@ -49,30 +50,31 @@ def parse_date(date:str)-> list:
 
 
 
-data_1 = parse_date('18 de Janeiro de 2023')
-data_2 = parse_date('18 de Janeiro de 2024')
-
-def time_delta(date_list1:list,date_list2:list)-> int:
+def time_delta(data_1:str,data_2:str)-> int:
     '''
 
- Parâmetros
+    Parameters
     ----------
-    date_list1 : list
-        Uma lista contendo os componentes da data no formato ["dia", "nome do mês", "ano"].
-    date_list2 : list
-        Uma lista contendo os componentes da data no formato ["dia", "nome do mês", "ano"].
+    data_1 : str
+        Uma string de formato "numero_dia de nome_mes de ano"
+        Exemplo_de_string: date = "28 de Agosto de 2023"
+    data_2 : str
+        Uma string de formato "numero_dia de nome_mes de ano"
+        Exemplo_de_string: date = "28 de Agosto de 2023"
 
-    Retorna
+    Returns
     -------
     int
         A diferença de tempo em dias entre as duas datas de entrada. Um valor positivo significa
-        que date_list2 está após date_list1, e um valor negativo significa que date_list2 está antes de date_list1.
+        que data_1 está após data_2, e um valor negativo significa que data_2 está antes de data_1.
     
     Teste com datas que se distanciam em 1 ano
-    >>> time_delta(data_1,data_2)
-    365 days, 0:00:00
+    >>> time_delta('18 de Janeiro de 2023','18 de Janeiro de 2024')
+    datetime.timedelta(days=365)
     
     '''
+    date_list1 = parse_date(data_1)
+    date_list2 = parse_date(data_2)
     dict_month = {"Janeiro":1, "Fevereiro":2, "Março":3, "Abril":4, 
                   "Maio":5, "Junho":6, "Julho":7, "Agosto":8, "Setembro":9, 
                   "Outubro":10, "Novembro":11, "Dezembro":12}
@@ -83,6 +85,20 @@ def time_delta(date_list1:list,date_list2:list)-> int:
 
     int_delta = dt_translated_date2-dt_translated_date1
     return int_delta
+
+def extract_lines(s_path:str)->list:
+    l_file = []
+    l_file2 = []
+    with open('input_text.txt') as f:
+        for line in f.readlines():
+            l_file.append(line)
+    l_file2 = l_file[1]
+    l_file = l_file[0]
+    return (l_file,l_file2)
+
+l_file = extract_lines('input_text.txt')[0]
+l_file2 = extract_lines('input_text.txt')[1]
+print(l_file)
 
 
 if __name__ == "__main__":
